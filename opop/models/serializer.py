df79dc9c67849acbfd899baac91efc85267f1922
parksooo <popomarin28@gmail.com>
2024-02-16 16:22:29 +0900

    feat : dto 생성

--- opop/models/serializer.py
+++ opop/models/serializer.py
@@ -1,15 +1,51 @@
 from rest_framework import serializers
 
-from .models import User, GameRoom
+from .models import User, GameRoom, MatchHistory
+
 
 class GameRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameRoom
+
+
 class UserSerializer(serializers.ModelSerializer):
     gameRooms = GameRoomSerializer(read_only=True, many=True)
+
     class Meta:
         model = User
-        fields = ['id', 'intra_name', 'gameRooms']
+        fields = ['id', 'intra_name', 'picture', 'gameRooms']
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MatchHistory
+        fields = ['opponent_name', 'winner']
+
+    def get_winner(self, obj):
+        if obj.result == 'WIN':
+            return obj.user.intra_name
+        elif obj.result == 'LOSE':
+            return obj.opponent_name.intra_name
+        else:
+            return None
+
+    def to_representation(self, instance):
+        representation = {
+            'opponent_name': instance.opponet_name,
+            'winner': self.get_winner(instance)
+        }
+        return representation
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+
+    gameRooms = GameRoomSerializer(read_only=True, many=True)
+    matchHistory = MatchSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
+        fields = ['total_win', 'total_lose', 'isBlack', 'matchHistory']
+
 
 class FrindDto(serializers.ModelSerializer):
     class Meta:
