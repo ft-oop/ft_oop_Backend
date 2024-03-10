4795ad8099f69e40189e69b19fd2e0d68cd02cbb
julien <nttmxk@korea.ac.kr>
2024-03-10 15:16:09 +0900

    fix: set test environments

--- opop/routing.py
+++ opop/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/main/', consumers.GameConsumer.as_asgi()),
+    path('ws/main/', consumers.OopConsumer.as_asgi()),
 ]
